3bfb0bbf3013fe4b02a1f6715356ab2cc2e7129f
minhaaj-t <mminhajmahroof@gmail.com>
2025-12-25 21:11:47 +0300

    final1

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   build: {
@@ -6,7 +7,13 @@ export default defineConfig({
     assetsDir: 'assets',
     rollupOptions: {
       input: {
-        main: './index.html'
+        main: resolve(__dirname, 'index.html'),
+        products: resolve(__dirname, 'products.html'),
+        contact: resolve(__dirname, 'contact.html'),
+        about: resolve(__dirname, 'about.html'),
+        cookies: resolve(__dirname, 'cookies.html'),
+        terms: resolve(__dirname, 'terms.html'),
+        privacy: resolve(__dirname, 'privacy.html')
       }
     }
   },
